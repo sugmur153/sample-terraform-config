093697fae52a9a9440afa310398fd7e3cbb13ffe
suganya153 <suganya.muralikrishnan93@gmail.com>
2025-03-26 19:00:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,15 @@ terraform {
     path = "terraform.tfstate"
   }
 }
- 
+
+# Declare the variables
+variable "var1" {}
+variable "var2" {}
+variable "var3" {}
+variable "var4" {}
+
+# Use the variables in the local file resource
 resource "local_file" "example" {
-  content  = "Hello, Terraform!"
-  filename = "${path.module}/hello.txt"
+  content  = "Hello, Terraform! Var1: ${var.var1}, Var2: ${var.var2}, Var3: ${var.var3}, Var4: ${var.var4}"
+  filename = "${path.module}/hello-${var.var3}.txt"
 }
